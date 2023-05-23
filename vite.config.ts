9af9f2e80d5ef8f7d671ea9afec6411c905cb1b0
jrojavid2 <jrojavid2@outlook.es>
2023-05-23 01:13:12 -0500

    add base config vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: ''
-  // root: './src/encuestas',
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //         main: resolve(__dirname, 'src/encuestas/index.html'),
-  //         multiminerales: resolve(__dirname, 'src/encuestas/some-page.html')
-  //     }
-  //   }
-  // }
+  base: '/encuestas-fiis/'
 })
