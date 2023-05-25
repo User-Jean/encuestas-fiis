69cd07f57884f65b1c1170620d646e861c2d3f2e
User-Jean <52880669+User-Jean@users.noreply.github.com>
2023-05-25 11:45:30 -0500

    Update DashHome.tsx

--- src/app/dash/pages/DashHome.tsx
+++ src/app/dash/pages/DashHome.tsx
@@ -46,7 +46,7 @@ export const DashHome: React.FC = () => {
 			field: 'actions',
 			headerName: 'Acciones',
 			renderCell: ({ row }: GridRenderCellParams) => (
-				<Operations encuesta={row} />
+				<Operations encuesta={row} loadData={loadData} />
 			),
 			minWidth: 180,
 		},
@@ -116,7 +116,7 @@ export const DashHome: React.FC = () => {
 				</Button> : ''}
 			</Box>
 			<Table rows={encuestas} columns={isAdmin ? columns : columnsUsers} />
-			<Modal open={isOpenModal} setOpen={setIsOpenModal} type={Type.Add} />
+			<Modal open={isOpenModal} setOpen={setIsOpenModal} loadData={loadData} type={Type.Add} />
 		</Stack>
 	);
 };
